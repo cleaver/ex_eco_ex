1114321ff10aaa9bf2da44024eceae0ddc00a91c
ogourment <ogourment@smarterportal.com>
2025-07-31 16:00:53 -0400

    added mix test.watch

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Ecoexpense.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:machete, "~> 0.3.11"}
+      {:machete, "~> 0.3.11"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
